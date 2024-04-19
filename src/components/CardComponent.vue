0a3e141ac6c6fabcfbc92b5558fc45ddca40bde4
Wim Bervoets <wim.bervoets@pxl.be>
2024-04-19 12:11:15 +0200

    deel8

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -2,6 +2,11 @@
     export default {
         props: {
             product: Object
+        },
+        methods: {
+            gotoProduct() {
+                this.$router.push({name: 'productParams', params: {price: this.product.price}, query: {title: this.product.title}})
+            }
         }
     }
 </script>
@@ -14,10 +19,12 @@
                     {{ product.title }}
             </h1>
             <p class="card-overlay-paragraph">Price: €{{ product.price }}</p>
+
+            <!-- zie ook _components.scss regel 113 - 116 voor aanpassingen aan de opmaak: -->
             <router-link class="card-overlay-button" :to="{ name: 'productParams', params: {price: product.price}, query: {title: product.title}}">
                 Info
             </router-link>
-            <button class="card-overlay-button">Order now</button>
+            <button class="card-overlay-button" @click="gotoProduct()">Order now</button>
         </div>
         <img :src="product.imageLink" :alt="product.imageAlt" class="card-image">
     </div>
